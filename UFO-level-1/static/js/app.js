61fe0c8737d3c74502e959dcf8addbe086277ac1
Christina Clements <crfc.tk@gmail.com>
2020-02-06 10:43:46 -0600

    table filled

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach(function(tableData) {
+    console.log(tableData);
+    var row = tbody.append("tr");
+
+    Object.entries(tableData).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
